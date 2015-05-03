4680eee65e06b2517a532edd8e047795b86cf871
Muhammadmateen <engr_muhammadmateen@hotmail.com>
2015-05-03 00:55:57 -0700

    Dynamic to app
    syelani assingment

---
+++ ToDoList/todo.js
@@ -0,0 +1,24 @@
+/**
+ * Created by Muhammad on 5/3/2015.
+ */
+/**
+ * Created by Muhammad on 5/2/2015.
+ */
+
+var app = angular.module("myFirebaseApp",["ngMaterial" , "firebase"]);
+
+app.controller("myForm", function($scope, $firebaseArray)
+{
+    var ref = new Firebase("https://mateentodo.firebaseio.com/");
+
+    $scope.messages = $firebaseArray(ref);
+
+    $scope.addvalue = function()
+    {
+        $scope.messages.$add(
+            {name:$scope.validName}
+        );
+        $scope.validName = "";
+    };
+
+});
